ec0c9b015925110b99fb5b8922c3a3db3fd28b1c
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-05-09 22:35:29 -0400

    Added association relationships to lesson, coach, and student, models.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ApplicationRecord
 	has_secure_password 
+	has_many :lessons
+	has_many :coaches, through: :lessons
 
 	def full_name
 		self.first_name.capitalize + " " + self.last_name.capitalize
