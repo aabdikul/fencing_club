73bf47f46cd039c76686cdafc03a381c5450a924
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-05-11 22:47:47 -0400

    Added ability to create a lesson by intaking coach_id, student_id, and datetime.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,17 +6,18 @@ class LessonsController < ApplicationController
 
 	def create 
 		@lesson = Lesson.new(lesson_params)
-		byebug
+		@lesson.save
+		redirect_to lesson_path(@lesson)
 	end
 
 	def show
-		@lesson = Lesson.find(params[:id])
-	end
+		@lesson = Lesson.find_by(id: params[:id])
+ 	end
 
 	private 
 
 	def lesson_params
-		params.require(:lesson).permit(:student_id, :coach_id, :date, :time)
+		params.require(:lesson).permit(:student_id, :coach_id, :date)
 	end
 
 end
