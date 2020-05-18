83c6683c3c4d338edba9ef29af3f01d227e61eea
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-05-17 20:33:16 -0400

    Added validations for student model and added full_messages readout to the _form

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,12 @@ class Student < ApplicationRecord
 	has_secure_password 
 	has_many :lessons
 	has_many :coaches, through: :lessons
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :age, numericality: { greater_than_or_equal_to: 5, message: "Student is not 5 years old or older."}
+	validates :weapon, inclusion: { in: %w(foil epee sabre),
+    message: "%{value} is not a valid weapon choice." }
+    validates :username, presence: true
 
 	def full_name
 		self.first_name.capitalize + " " + self.last_name.capitalize
